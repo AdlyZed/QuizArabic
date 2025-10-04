b3b25849088198c2026977b9389f5d03b357647a
AdlyZed <81880301+AdlyZed@users.noreply.github.com>
2025-10-04 16:07:43 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const urlsToCache = [
   "/",
   "/index.html",
   "/manifest.json",
-  "/favicon.png"
+  "/alif-icon.png"
   // add CSS, JS, JSON data files here
 ];
 
@@ -18,3 +18,4 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
